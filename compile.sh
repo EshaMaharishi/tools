5b60c85f86faa8ad9a7ce74f6a754d5ba57ce13d
Esha Maharishi <esha.maharishi@mongodb.com>
2016-03-07 10:27:08 -0500

    use clang compiler

--- compile.sh
+++ compile.sh
@@ -1,12 +1,15 @@
 set -e
 
 # regular parallel compile
-#time scons -j 24 core
+#CC=$(which clang) CXX=$(which clang++) time scons -j 24 --quiet core
 
 # compile with gdbserver
-#time scons --allocator=system --opt=off --gdbserver -j 24 core
+#CC=$(which clang) CXX=$(which clang++) time scons --allocator=system --quiet --opt=off --gdbserver -j 24 core
+#CC=$(which clang) CXX=$(which clang++) time scons --allocator=system --quiet --opt=off --gdbserver -j 24 build/opt/mongo/db/s/metadata_test
 
 # compile with address sanitizer
-ASAN_SYMBOLIZER_PATH=$(which llvm-symbolizer) time scons --allocator=system --opt=off --sanitize=address -j 24 core
+#CC=$(which clang) CXX=$(which clang++) ASAN_SYMBOLIZER_PATH=$(which llvm-symbolizer) time scons  --quiet --allocator=system --opt=off --sanitize=address -j 24 core
+
+CC=$(which clang) CXX=$(which clang++) ASAN_SYMBOLIZER_PATH=$(which llvm-symbolizer) time scons  --quiet --allocator=system --opt=off --sanitize=address -j 24 unittests
 
 printf "last compile on branch\n`git branch`\nwith changed files\n`git diff --name-only master`\nwith diff from master\n`git diff master`" > esha/last_compile
